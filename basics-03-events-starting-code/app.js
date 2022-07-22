d521c6c68593563dc53e3edc0e0ffb7f2c6123a5
ltizzi <terlizzileonardo@gmail.com>
2022-07-22 20:22:07 -0300

    basics2-ejercisio 2

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -2,8 +2,22 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      num: 5,
+      username: "",
     };
+  },
+  methods: {
+    add(num) {
+      this.counter += num; //counter++
+    },
+    remove(num) {
+      this.counter -= num; //counter--
+    },
+    changeUsername(event) {
+      console.log(event);
+      this.username = event.target.value;
+    },
   },
 });
 
-app.mount('#events');
+app.mount("#events");
