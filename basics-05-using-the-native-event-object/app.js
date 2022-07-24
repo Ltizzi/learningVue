3e8ea4e61035cc0096010c00a4fcf4b41aacb89b
ltizzi <terlizzileonardo@gmail.com>
2022-07-24 19:53:43 -0300

    fin clase 20

--- basics-05-using-the-native-event-object/app.js
+++ basics-05-using-the-native-event-object/app.js
@@ -3,11 +3,34 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      //fullname: "",
+      lastname: "",
     };
+  },
+  watch: {
+    counter(value) {
+      value > 50 ? (this.counter = 0) : console.log("menor que 50");
+    },
+    // name(value) {
+    //   this.fullname = value + " " + this.lastname;
+    // },
+    // lastname(value) {
+    //   this.fullname = this.name + " " + value;
+    // },
+  },
+  computed: {
+    fullname() {
+      return this.name === "" || this.lastname === ""
+        ? ""
+        : this.name + " " + this.lastname;
+    },
   },
   methods: {
     setName(event) {
       this.name = event.target.value;
+    },
+    setLastname(event) {
+      this.lastname = event.target.value;
     },
     add(num) {
       this.counter = this.counter + num;
@@ -18,6 +41,7 @@ const app = Vue.createApp({
     },
     resetInput() {
       this.name = "";
+      this.fullname = "";
     },
   },
 });
