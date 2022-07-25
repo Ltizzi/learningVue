8f565ca6c70144359b0510616a19ae18bc11abd1
ltizzi <terlizzileonardo@gmail.com>
2022-07-24 22:09:43 -0300

    v-if v-for terminado

--- 2 - rendering conditional components and lists/lists-cond-01-starting-setup/app.js
+++ 2 - rendering conditional components and lists/lists-cond-01-starting-setup/app.js
@@ -1,7 +1,23 @@
 const app = Vue.createApp({
   data() {
-    return { goals: [] };
+    return {
+      goals: [],
+      enteredGoalValue: "",
+    };
+  },
+  computed: {
+    isEmpty() {
+      return this.goals.length === 0 ? true : false;
+    },
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+    },
+    removeGoal(index) {
+      this.goals.splice(index, 1);
+    },
   },
 });
 
-app.mount('#user-goals');
+app.mount("#user-goals");
