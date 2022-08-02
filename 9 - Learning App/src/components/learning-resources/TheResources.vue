2bcd55edfc616bc2016f49a9ee834207056f7e6a
ltizzi <terlizzileonardo@gmail.com>
2022-08-02 18:50:02 -0300

    learning app advance

---
+++ 9 - Learning App/src/components/learning-resources/TheResources.vue
@@ -0,0 +1,82 @@
+<template lang="">
+  <base-card>
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
+      >Stored Resources</base-button
+    >
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <!-- DINAMIC CUSTOM COMPONENT -->
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 1,
+          title: 'Google',
+          description: 'Everybody needs to know how to google things',
+          link: 'https://www.google.com',
+        },
+        {
+          id: 2,
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource,
+    };
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat';
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat';
+    },
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    getLastId() {
+      return this.storedResources.length;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: this.getLastId + 1,
+        title: title,
+        description: description,
+        link: url,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
+    },
+  },
+};
+</script>
+
+<style lang=""></style>
