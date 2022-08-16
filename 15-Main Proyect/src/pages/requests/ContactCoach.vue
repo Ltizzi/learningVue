5536ce49dbfd91c11f9bec1def2a4f25c2c26849
ltizzi <terlizzileonardo@gmail.com>
2022-08-16 12:57:59 -0300

    proyect progress

---
+++ 15-Main Proyect/src/pages/requests/ContactCoach.vue
@@ -0,0 +1,5 @@
+<template lang="">CONTACT A COACH</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
---
+++ 15-Main Proyect/src/pages/requests/RequestsReceived.vue
@@ -0,0 +1,5 @@
+<template lang="">REQUESTS</template>
+<script>
+export default {};
+</script>
+<style lang=""></style>
